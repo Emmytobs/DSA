aaafc26e8d5a6c18b7951291bb07fe2a971e089f
Emmanuel Otobo <eotobo11@gmail.com>
2024-10-23 09:32:34 -0230

    py solution for 647

---
+++ leetcode-solutions/647_palindromic_substrings/solution.py
@@ -0,0 +1,20 @@
+# This problem is very similar to #5: https://leetcode.com/problems/longest-palindromic-substring/
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        output = 0
+
+        for i in range(len(s)):
+            # Returns the palindromes for odd-length substrings
+            count_for_odd = self.count_palindromes(s, i, i)
+            # Returns the palindromes for even-length substrings
+            count_for_even = self.count_palindromes(s, i, i + 1)
+            output += (count_for_odd + count_for_even)
+        return output
+    
+    def count_palindromes(self, s, l, r):
+        no_of_palindromes = 0
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            no_of_palindromes += 1
+            l -= 1
+            r += 1
+        return no_of_palindromes
