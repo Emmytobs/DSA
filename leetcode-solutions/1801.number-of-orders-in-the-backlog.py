f7d826fbbc710cde30d9c61621ecb0e989263f89
Emmanuel Otobo <eotobo11@gmail.com>
2024-11-13 21:07:00 -0330

    Added py solution for #1801

---
+++ leetcode-solutions/1801.number-of-orders-in-the-backlog.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=1801 lang=python3
+#
+# [1801] Number of Orders in the Backlog
+#
+
+# @lc code=start
+import heapq
+from typing import List
+
+class Solution:
+    def getNumberOfBacklogOrders(self, orders: List[List[int]]) -> int:
+        sell, buy = [], []
+        for p, a, t in orders:
+            if t == 0:
+                heapq.heappush(buy, [-p, a])
+            else:
+                heapq.heappush(sell, [p, a])
+            while sell and buy and sell[0][0] <= -buy[0][0]:
+                k = min(buy[0][1], sell[0][1])
+                buy[0][1] -= k
+                sell[0][1] -= k
+                if buy[0][1] == 0: heapq.heappop(buy)
+                if sell[0][1] == 0: heapq.heappop(sell)
+        return sum(a for _, a in buy + sell) % (10**9 + 7)
+# @lc code=end
