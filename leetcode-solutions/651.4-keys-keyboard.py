9dc03c2f445905c6543686276c9182c651d38af4
Emmanuel Otobo <eotobo11@gmail.com>
2024-11-26 10:59:32 -0330

    Added py solution for #651

---
+++ leetcode-solutions/651.4-keys-keyboard.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=651 lang=python3
+#
+# [651] 4 Keys Keyboard
+#
+
+# @lc code=start
+from collections import Counter
+
+class Solution:
+    def maxA(self, n: int) -> int:
+# @lc code=end
+
